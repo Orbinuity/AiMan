eee1348ca82b1a75e2371ba8a63818e4db175aba
Flurop <flora@ratgers.nl>
2026-08-07 09:33:04 +0200

    Correct color now

--- uninstall.sh
+++ uninstall.sh
@@ -7,5 +7,5 @@ if [ -f "$TARGET" ]; then
     rm -f "$TARGET"
     printf "\033[0;32m[✓]\033[0m AiMan has been uninstalled.\n";
 else
-    printf "\033[0;32m[!]\033[0m AiMan was not found in $HOME/.local/bin\n"
+    printf "\033[1;33m[!]\033[0m AiMan was not found in $HOME/.local/bin\n"
 fi
