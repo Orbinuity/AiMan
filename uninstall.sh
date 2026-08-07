cfbaadc969a826b83364c508e4518d42dcf5a074
Flurop <flora@ratgers.nl>
2026-08-07 21:42:10 +0200

    Added Android support

--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,12 @@
 set -e
 
 TARGET="$HOME/.local/bin/aiman"
+APP_DIR="$HOME/.local/share/AiMan"
 
-if [ -f "$TARGET" ]; then
+if [ -f "$TARGET" ] || [ -d "$APP_DIR" ]; then
     rm -f "$TARGET"
-    printf "\033[0;32m[✓]\033[0m AiMan has been uninstalled.\n";
+    rm -rf "$APP_DIR"
+    printf "\033[0;32m[✓]\033[0m AiMan has been uninstalled.\n"
 else
     printf "\033[1;33m[!]\033[0m AiMan was not found in $HOME/.local/bin\n"
 fi
