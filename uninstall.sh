29cdbe434a51409616bec76f5e63ac3c8007294a
Flurop <flora@ratgers.nl>
2026-08-07 09:31:26 +0200

    Added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+TARGET="$HOME/.local/bin/aiman"
+
+if [ -f "$TARGET" ]; then
+    rm -f "$TARGET"
+    printf "\033[0;32m[✓]\033[0m AiMan has been uninstalled.\n";
+else
+    printf "\033[0;32m[!]\033[0m AiMan was not found in $HOME/.local/bin\n"
+fi
